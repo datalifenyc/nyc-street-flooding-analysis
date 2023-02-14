ebd42397ff8b640dd70e0162325422221aecf259
Chidi <57526824+datalifenyc@users.noreply.github.com>
2023-02-14 18:43:03 -0500

    Preview data to test geopandas compatibility and export current environment requirements

--- analysis-book/_build/jupyter_execute/get-data.py
+++ analysis-book/_build/jupyter_execute/get-data.py
@@ -18,6 +18,7 @@
 # In[1]:
 
 
+import pyproj
 import geopandas as gpd
 
 
@@ -61,7 +62,19 @@ street_flooding_gdf = gpd.read_file(NYC_OPEN_DATA_311_API_GEOJSON, driver='GeoJS
 # In[4]:
 
 
+print(f'Total Observations: {len(street_flooding_gdf)}')
+
+
+# In[5]:
+
+
 street_flooding_gdf.head(10)
+
+
+# In[6]:
+
+
+street_flooding_gdf.info()
 
 
 # In[ ]:
@@ -69,3 +82,154 @@ street_flooding_gdf.head(10)
 
 
 
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[7]:
+
+
+nybb_df = gpd.read_file(gpd.datasets.get_path('nybb'))
+# nybb_df.set_crs(epsg=3857, inplace=True)
+
+
+# In[8]:
+
+
+nybb_df.info()
+
+
+# In[9]:
+
+
+nybb_df = nybb_df.set_index("BoroName")
+
+
+# In[10]:
+
+
+nybb_df['area'] = nybb_df.area
+
+
+# In[11]:
+
+
+nybb_df['boundary'] = nybb_df.boundary
+
+
+# In[12]:
+
+
+nybb_df['centroid'] = nybb_df.centroid
+
+
+# In[13]:
+
+
+nybb_df.plot('area', legend=True)
+
+
+# In[14]:
+
+
+nybb_df.explore("area", legend=False)
+
+
+# In[15]:
+
+
+nybb_df.index
+
+
+# In[16]:
+
+
+nybb_df.columns
+
+
+# In[17]:
+
+
+nybb_df.index
+
+
+# In[18]:
+
+
+nybb_df.head()
+
+
+# In[19]:
+
+
+nybb_df.dtypes
+
+
+# In[20]:
+
+
+nybb_df.info()
+
+
+# In[21]:
+
+
+print(type(list(nybb_df.index)))
+
+
+# In[22]:
+
+
+nybb = gpd.read_file(gpd.datasets.get_path('nybb'))
+
+
+# In[23]:
+
+
+nybb.explore()
+
+
+# In[24]:
+
+
+nybb.explore(
+     column="BoroName", # make choropleth based on "BoroName" column
+     tooltip="BoroName", # show "BoroName" value in tooltip (on hover)
+     popup=True, # show all values in popup (on click)
+     tiles="CartoDB positron", # use "CartoDB positron" tiles
+     cmap="Set1", # use "Set1" matplotlib colormap
+     style_kwds=dict(color="black") # use black outline
+    )
+
+
+# ## References
+# 
+# ### GeoPandas
+# 
+# [Reading and Writing Files | GoePandas Documentation](https://geopandas.org/en/stable/docs/user_guide/io.html)
+# 
+# ### pyproj
+# 
+# [On fresh Conda installation of PyProj: pyproj unable to set database path. _pyproj_global_context_initialize()](https://stackoverflow.com/questions/69630630/on-fresh-conda-installation-of-pyproj-pyproj-unable-to-set-database-path-pypr)
+# 
+# #### Fix
+# 
+# Un-install pyproj
+# 
+# `conda remove --force pyproj`
+# 
+# Re-install pyproj via `pip` instead of `conda`
+# 
+# `pip install pyproj`
+# 
+
+# 
